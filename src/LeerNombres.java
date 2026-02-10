ed9f19c1645e670febfcd92b44820977bd91e483
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-02-10 12:10:28 +0100

    subir2

--- src/LeerNombres.java
+++ src/LeerNombres.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LeerNombres {
+    public static void leer(String fichero){ //Método para leer el archivo
+        try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el fichero --> "+ e.getMessage());
+        }
+    }
 }
--- src/NombreMain.java
+++ src/NombreMain.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class NombreMain {
+    public static void main(String[] args) {
+        String fichero = "nombres.txt";
+        LeerNombres.leer(fichero); //Llamar al método
+    }
 }
